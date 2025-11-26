7dbc0c198fd9584978d726e04bf4878acbf32882
ezegaricas-prog <ezegaricas@gmail.com>
2025-11-26 05:08:20 +0000

    Week 07
    Week07 assignments uploaded.

---
+++ Module7-Barrientos/Module6MethodsProjectDL/frmRealID.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Module6MethodsProjectDL
+{
+    partial class frmRealID
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.txtLeftOperand = new System.Windows.Forms.TextBox();
+            this.txtRightOperand = new System.Windows.Forms.TextBox();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnModulus = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLeftOperand
+            // 
+            this.txtLeftOperand.Font = new System.Drawing.Font("Times New Roman", 16F);
+            this.txtLeftOperand.Location = new System.Drawing.Point(160, 10);
+            this.txtLeftOperand.Name = "txtLeftOperand";
+            this.txtLeftOperand.Size = new System.Drawing.Size(120, 32);
+            this.txtLeftOperand.TabIndex = 0;
+            // 
+            // txtRightOperand
+            // 
+            this.txtRightOperand.Font = new System.Drawing.Font("Times New Roman", 16F);
+            this.txtRightOperand.Location = new System.Drawing.Point(160, 50);
+            this.txtRightOperand.Name = "txtRightOperand";
+            this.txtRightOperand.Size = new System.Drawing.Size(120, 32);
+            this.txtRightOperand.TabIndex = 1;
+            // 
+            // lblLeft
+            // 
+            this.lblLeft.AutoSize = true;
+            this.lblLeft.Font = new System.Drawing.Font("Times New Roman", 16F);
+            this.lblLeft.Location = new System.Drawing.Point(30, 12);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(128, 25);
+            this.lblLeft.Text = "Left Operand";
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Font = new System.Drawing.Font("Times New Roman", 16F);
+            this.lblRight.Location = new System.Drawing.Point(30, 52);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(133, 25);
+            this.lblRight.Text = "Right Operand";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.Location = new System.Drawing.Point(10, 100);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(40, 40);
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.btnSubtract.Location = new System.Drawing.Point(60, 100);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(40, 40);
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.btnMultiply.Location = new System.Drawing.Point(110, 100);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(40, 40);
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.btnDivide.Location = new System.Drawing.Point(160, 100);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(40, 40);
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnModulus
+            // 
+            this.btnModulus.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold);
+            this.btnModulus.Location = new System.Drawing.Point(210, 100);
+            this.btnModulus.Name = "btnModulus";
+            this.btnModulus.Size = new System.Drawing.Size(40, 40);
+            this.btnModulus.Text = "%";
+            this.btnModulus.UseVisualStyleBackColor = true;
+            this.btnModulus.Click += new System.EventHandler(this.btnModulus_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnClear.Location = new System.Drawing.Point(10, 150);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(80, 35);
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.btnExit.Location = new System.Drawing.Point(120, 150);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(80, 35);
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BackColor = System.Drawing.Color.FromArgb(255, 255, 192);
+            this.lblDisplay.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold);
+            this.lblDisplay.Location = new System.Drawing.Point(10, 200);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(240, 80);
+            this.lblDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmRealID
+            // 
+            this.ClientSize = new System.Drawing.Size(280, 300);
+            this.Controls.Add(this.txtLeftOperand);
+            this.Controls.Add(this.txtRightOperand);
+            this.Controls.Add(this.lblLeft);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnModulus);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblDisplay);
+            this.Name = "frmRealID";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Valid ID Required";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLeftOperand;
+        private System.Windows.Forms.TextBox txtRightOperand;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnModulus;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblDisplay;
+    }
+}
---
+++ Module7-Barrientos/Module6MethodsProjectDL/frmRealID.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Module6MethodsProjectDL
+{
+    public partial class frmRealID : Form
+    {
+        public frmRealID()
+        {
+            InitializeComponent();
+
+            // Set form properties
+            this.Text = "Valid ID Required";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.LightBlue; // change to any color you like
+
+            // Set label font smaller for validation messages
+            lblDisplay.Font = new Font(lblDisplay.Font.FontFamily, 10, FontStyle.Bold);
+            lblDisplay.Text = "";
+        }
+
+        // Button event handlers
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            PerformOperation('+');
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            PerformOperation('-');
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            PerformOperation('*');
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            PerformOperation('/');
+        }
+
+        private void btnModulus_Click(object sender, EventArgs e)
+        {
+            PerformOperation('%');
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLeftOperand.Text = "";
+            txtRightOperand.Text = "";
+            lblDisplay.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Shared method for calculations and validation
+        private void PerformOperation(char op)
+        {
+            lblDisplay.ForeColor = Color.Black; // valid text color
+            List<string> errors;
+            double left, right;
+
+            bool valid = ValidateOperands(out left, out right, op, out errors);
+
+            if (!valid)
+            {
+                lblDisplay.ForeColor = Color.Red;
+                lblDisplay.Text = string.Join(Environment.NewLine, errors);
+                return;
+            }
+
+            try
+            {
+                double result = Calculate(left, right, op);
+                lblDisplay.ForeColor = Color.Black;
+                lblDisplay.Text = "Result: " + result;
+            }
+            catch (DivideByZeroException)
+            {
+                lblDisplay.ForeColor = Color.Red;
+                lblDisplay.Text = "Error: Division or modulus by zero is not allowed.";
+            }
+            catch (Exception ex)
+            {
+                lblDisplay.ForeColor = Color.Red;
+                lblDisplay.Text = "Unexpected error: " + ex.Message;
+            }
+        }
+
+        private bool ValidateOperands(out double left, out double right, char op, out List<string> errors)
+        {
+            errors = new List<string>();
+            left = right = 0;
+
+            string leftText = txtLeftOperand.Text.Trim();
+            string rightText = txtRightOperand.Text.Trim();
+
+            if (string.IsNullOrEmpty(leftText))
+                errors.Add("Left operand cannot be empty.");
+            if (string.IsNullOrEmpty(rightText))
+                errors.Add("Right operand cannot be empty.");
+
+            bool leftParsed = double.TryParse(leftText, out left);
+            if (!leftParsed && !string.IsNullOrEmpty(leftText))
+                errors.Add("Left operand must be a number.");
+
+            bool rightParsed = double.TryParse(rightText, out right);
+            if (!rightParsed && !string.IsNullOrEmpty(rightText))
+                errors.Add("Right operand must be a number.");
+
+            if ((op == '/' || op == '%') && rightParsed && right == 0)
+                errors.Add("Right operand cannot be zero for division or modulus.");
+
+            if (op == '%' && ((leftParsed && left < 0) || (rightParsed && right < 0)))
+            {
+                if (left < 0) errors.Add("Left operand must be non-negative for modulus.");
+                if (right < 0) errors.Add("Right operand must be non-negative for modulus.");
+            }
+
+            return errors.Count == 0;
+        }
+
+        private double Calculate(double left, double right, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return left + right;
+                case '-':
+                    return left - right;
+                case '*':
+                    return left * right;
+                case '/':
+                    if (right == 0) throw new DivideByZeroException();
+                    return left / right;
+                case '%':
+                    if (right == 0) throw new DivideByZeroException();
+                    return left % right;
+                default:
+                    throw new InvalidOperationException("Unknown operation");
+            }
+        }
+    }
+}
